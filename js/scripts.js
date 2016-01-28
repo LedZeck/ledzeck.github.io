1c5cb38b4979ff59384b1417a4fd09aaaa51e4bf
Zeckolas <zeckamtwo@gmail.com>
2016-01-27 23:40:32 -0300

    uptade

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//VEGAS BACKGROUND
+
+$(function() {
+    $('#bg').vegas({
+        delay: 10000,
+    timer: false,
+    shuffle: false,
+    transition: '',
+    transitionDuration: 4000,
+    overlay: false,
+        slides: [
+            { src: 'img/background.jpg' }
+        ],
+    });
+})
+
+// ANIMATIONS
+
+$(window).scroll(function(){
+    $('.circle').each(function(){
+        var imagePos = $(this).offset().top;
+
+        var topOfWindow = $(window).scrollTop();
+            if(imagePos < topOfWindow+500){
+                $(this).addClass("expandUp");
+            }
+    });
+});
