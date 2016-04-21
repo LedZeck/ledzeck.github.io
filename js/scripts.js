6408e8f26e33ab0826f2eaceef479e40c5dbb073
LedZeck <zeckamtwo@gmail.com>
2016-04-20 22:14:30 -0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -21,7 +21,7 @@ $(window).scroll(function(){
         var imagePos = $(this).offset().top;
 
         var topOfWindow = $(window).scrollTop();
-            if(imagePos < topOfWindow+500){
+            if(imagePos < topOfWindow+600){
                 $(this).addClass("expandUp");
             }
     });
